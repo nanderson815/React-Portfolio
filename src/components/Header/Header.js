6bb112e1b5b52cb1c5c4b025453611b062bb7907
Noah A <nanderson815@gmail.com>
2019-03-18 17:18:28 -0400

    setting up files

---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './Header.module.css';
+
+const header = (props) => {
+    return (
+        <div id={styles.headerImage} className="valign-wrapper">
+            <div className="row container center">
+                <div>
+                    <h2 id={styles.TitleText} className="headerTxt white-text slideRight">Hello, I'm <span
+                        className="blue-text text-accent-2">NoahAnderson</span>.</h2>
+                    <h4 className="headerTxt white-text slideLeft">I'm a full-stack web developer. </h4>
+                    <a href="#projectsCont" className="waves-effect waves-light blue darken-4 btn-large fadeIn">&lt; View my
+                    Work /&gt;</a>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default header;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navbar from '../components/Navbar/Navbar';
+import Header from '../components/Header/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navbar></Navbar>
+        <Header />
         <header className="App-header">
          <h1>Hello, I am a react app.</h1>
         </header>
