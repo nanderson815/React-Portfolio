b970e30fb46a3daf4768e1cd6b6e3800ef574245
Noah A <nanderson815@gmail.com>
2019-03-19 13:20:22 -0400

    adding about me section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,40 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import styles from './About.module.css';
 import Skills from './Skills/Skills';
 
-const about = (props) => {
-    return (
-        <div id={styles.skillsCont} className="section grey lighten-5">
-            <div className="container">
-                <div className="row">
-                    <h2 id={styles.skillsHeader} className="header">About</h2>
-                </div>
+class About extends Component {
 
-                <div id="skillsContainer" className="row">
-                    <Skills
-                        style={[styles.skillIcons, "material-icons"].join(' ')}
-                        icon="person_pin"
-                        title="User Focused"
-                        desc="I understand that it's not enough for your project to just 'work'. It needs to work beautifully."
-                    />
-                    <Skills
-                        style={[styles.skillIcons, "material-icons"].join(' ')}
-                        icon="group"
-                        title="Team Oriented"
-                        desc="I have extensive experience working in groups to deliver the best possible service and results
-                            for every stakeholder."
-                    />
-                    <Skills
-                        style={[styles.skillIcons, "material-icons"].join(' ')}
-                        icon="settings"
-                        title="Results Driven"
-                        desc="No matter what the task, I ensure that it is completed to the best of
-                            my ability."
-                    />
+    render() {
+        return (
+            <div id={styles.skillsCont} className="section grey lighten-5">
+                <div className="container">
+                    <div className="row">
+                        <h2 id={styles.skillsHeader} className="header">About</h2>
+                    </div>
+                    <div id="skillsContainer" className="row">
+                        {this.props.skills.map((skill, index) => {
+                            return (
+                                <Skills
+                                    style={[styles.skillIcons, "material-icons"].join(' ')}
+                                    icon={skill.icon}
+                                    title={skill.title}
+                                    desc={skill.desc}
+                                    key={skill.id}
+                                />
+                            )
+                        })}
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
+
 };
 
-export default about;
+export default About;
--- src/containers/App.js
+++ src/containers/App.js
@@ -6,6 +6,14 @@ import About from '../components/About/About';
 class App extends Component {
   state = {
     isTop: true,
+    skills: [
+      {
+        id: '1', icon: 'person_pin', title: 'User Focused', desc: "I understand that it's not enough for your project to just 'work'.It needs to work beautifully." },
+      {
+        id: '2', icon: 'group', title: 'Team Oriented', desc: 'I have extensive experience working in groups to deliver the best possible service and results for every stakeholder.' },
+      {
+        id: '3', icon: 'settings', title: 'Results Driven', desc: 'No matter what the task, I ensure that it is completed to the best of my ability.' },
+    ]
   };
 
   componentDidMount() {
@@ -18,12 +26,12 @@ class App extends Component {
   }
 
   render() {
-    
+
     return (
       <div className="App">
-        <Header scroll={this.state.isTop}/>
-        <About />
-        
+        <Header scroll={this.state.isTop} />
+        <About skills={this.state.skills}/>
+
       </div>
     );
   }
