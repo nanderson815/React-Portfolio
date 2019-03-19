8bb691bc3c9ad4554def43c8b64156a7d4518398
Noah A <nanderson815@gmail.com>
2019-03-19 13:41:58 -0400

    adding about me section

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -12,7 +12,7 @@ class About extends Component {
                         <h2 id={styles.skillsHeader} className="header">About</h2>
                     </div>
                     <div id="skillsContainer" className="row">
-                        {this.props.skills.map((skill, index) => {
+                        {this.props.skills.map((skill) => {
                             return (
                                 <Skills
                                     style={[styles.skillIcons, "material-icons"].join(' ')}
@@ -22,7 +22,19 @@ class About extends Component {
                                     key={skill.id}
                                 />
                             )
-                        })}
+                        })
+                        }
+                    </div>
+                    <div id="aboutMeRow" class="row">
+                        <div class="col s12 m4">
+                            <div class="center">
+                                <img alt="Noah Anderson" id={styles.bioImage} class={styles.shadowed} src="./images/professionalPhoto2.png" />
+                                <h4>About Me</h4>
+                                <p class="light center">Full-Stack web developer and financial professional in Atlanta.
+                                    I am passionate about using technology to solve financial problems.
+                        </p>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
