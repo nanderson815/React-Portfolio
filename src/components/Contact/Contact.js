9676db5937743cbee294b47f69e66b780738bc39
Noah A <nanderson815@gmail.com>
2019-03-20 19:38:02 -0400

    adding form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,18 +1,77 @@
 import React from 'react';
 import styles from './Contact.module.css';
 
-const contact = (props) => {
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstName: '',
+            lastName: '',
+            email: '',
+            message: ''
+        };
 
-    return (
-        <div id={styles.contactCont} className="section grey lighten-5">
-            <div className="row container ">
-                <div className="headerContainer">
-                    <h2 id={styles.contactHeader} className="header">Contact</h2>
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+    handleSubmit(event) {
+        console.log(this.state);
+        event.preventDefault();
+    }
+
+    render() {
+        return (
+            <div id={styles.contactCont} className="section grey lighten-5">
+                <div className="row container ">
+                    <div className="headerContainer">
+                        <h2 id={styles.contactHeader} className="header">Contact</h2>
+                    </div>
+
+                    <form onSubmit={this.handleSubmit} className="col s12">
+                        <div className="row">
+                            <div className="input-field col s6">
+                                <input name="firstName" type="text" value={this.state.firstName} onChange={this.handleChange} />
+                                <label>
+                                    First Name
+                                </label>
+                            </div>
+                            <div className="input-field col s6">
+                                <input name="lastName" type="text" value={this.state.lastName} onChange={this.handleChange} />
+                                <label>
+                                    Last Name
+                                </label>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <input name="email" type="email" value={this.state.email} onChange={this.handleChange} />
+                                <label>
+                                    Email
+                                </label>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="input-field col s12">
+                                <textarea name="message" className="materialize-textarea" value={this.state.message} onChange={this.handleChange} />
+                                <label>
+                                    Your Message
+                                </label>
+                            </div>
+                        </div>
+
+                        <button type="submit" value="Submit" className="btn waves-effect waves-light blue darken-4">
+                            Submit</button>
+
+                    </form>
                 </div>
-            </div>
-        </div>
-    )
-
+            </div >
+        );
+    }
 }
 
-export default contact;
+export default Contact;
